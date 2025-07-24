1bfaa23b37b92711f4e3100d9b51969331650303
Balaji <addubalaji@gmail.com>
2025-07-24 12:26:57 +0530

    splash screen completed

--- lib/Splashscreen/splashScreen.dart
+++ lib/Splashscreen/splashScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mana_driver/Widgets/colors.dart';
+import 'package:mana_driver/Widgets/customText.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,8 +10,54 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  double dynamicHeight(BuildContext context, double figmaHeight) {
+    const baseHeight = 812;
+    final screenHeight = MediaQuery.of(context).size.height;
+    return (figmaHeight / baseHeight) * screenHeight;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(backgroundColor: korangeColor);
+    return Scaffold(
+      backgroundColor: korangeColor,
+      body: Column(
+        children: [
+          SizedBox(height: dynamicHeight(context, 296)),
+
+          Center(
+            child: Image.asset(
+              'images/app_logo.png',
+              width: 260,
+              height: 260,
+              fit: BoxFit.contain,
+            ),
+          ),
+
+          SizedBox(height: dynamicHeight(context, 470 - 296)),
+
+          Center(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomText(
+                  text: 'Made in India',
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  textcolor: kwhiteColor,
+                ),
+                SizedBox(width: 5),
+                Image.asset(
+                  'images/flag.png',
+                  width: 21,
+                  height: 17,
+                  fit: BoxFit.contain,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
