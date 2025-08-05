f98293cc6ab1fe9eb585ea266dfb7331d248958f
Balaji8555 <addubalaji@gmail.com>
2025-08-05 23:49:20 +0530

    firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'package:mana_driver/Splashscreen/splashScreen.dart';
+import 'package:mana_driver/firebase_options.dart';
 
-void main() {
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
